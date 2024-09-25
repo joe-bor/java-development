c4953355736b0785036a404e8137364b2c169024
Joezari Borlongan <bor.joezari@gmail.com>
2024-09-25 11:40:44 -0700

    intro

---
+++ Week1-IntroductionToJava/workbook-1/ShoppingList/src/main/java/com/pluralsight/ShoppingList.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class ShoppingList {
+    public static void main(String[] args) {
+        String[] groceryList = {
+                "apple",
+                "banana",
+                "watermelon"
+        };
+
+        for (String s : groceryList) {
+            System.out.println(s);
+        }
+    }
+}

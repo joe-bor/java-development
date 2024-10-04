973c962d9f25c9f50c82c5c2d3f94de540a87d9d
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-04 11:14:40 -0700

    CellPhoneService app exercise 2

--- workbook-2/src/main/java/com/pluralsight/CellPhone.java
+++ workbook-2/src/main/java/com/pluralsight/CellPhone.java
@@ -56,4 +56,8 @@ public class CellPhone {
     public void setOwner(String owner) {
         this.owner = owner;
     }
+
+    public void dial(String phoneNumber){
+        System.out.println(this.getOwner() + "'s phone is calling " + phoneNumber);
+    }
 }
--- workbook-2/src/main/java/com/pluralsight/CellPhoneService.java
+++ workbook-2/src/main/java/com/pluralsight/CellPhoneService.java
@@ -7,15 +7,16 @@ public class CellPhoneService {
     static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
         CellPhone cellPhone = new CellPhone();
+        createPhoneViaPrompt(cellPhone);
 
-        cellPhone.setSerialNumber((Integer.parseInt(promptUser("What is the serial number? "))));
-        cellPhone.setModel(promptUser("What model is the phone? "));
-        cellPhone.setCarrier(promptUser("Who is the carrier? "));
-        cellPhone.setPhoneNumber(promptUser("What is the phone number? "));
-        cellPhone.setOwner(promptUser("Who is the owner of the phone? "));
+        CellPhone cellPhone2 = new CellPhone();
+        createPhoneViaPrompt(cellPhone2);
 
-        printPhoneFields(cellPhone);
+        display(cellPhone);
+        display(cellPhone2);
 
+        cellPhone.dial(cellPhone2.getPhoneNumber());
+        cellPhone2.dial(cellPhone.getPhoneNumber());
     }
 
     static String promptUser(String message){
@@ -23,11 +24,20 @@ public class CellPhoneService {
         return scanner.nextLine().trim();
     }
 
-    static void printPhoneFields(CellPhone cellPhone){
+    static void display(CellPhone cellPhone){
+        System.out.println("\n----- Cell Phone Details -----");
         System.out.println("Serial Number: " + cellPhone.getSerialNumber());
         System.out.println("Model: " + cellPhone.getModel());
         System.out.println("Carrier: " + cellPhone.getCarrier());
         System.out.println("Phone number: " + cellPhone.getPhoneNumber());
-        System.out.println("Owner: " + cellPhone.getOwner());
+        System.out.println("Owner: " + cellPhone.getOwner() + "\n");
+    }
+
+    static void createPhoneViaPrompt(CellPhone cellPhone){
+        cellPhone.setSerialNumber((Integer.parseInt(promptUser("\nWhat is the serial number? "))));
+        cellPhone.setModel(promptUser("What model is the phone? "));
+        cellPhone.setCarrier(promptUser("Who is the carrier? "));
+        cellPhone.setPhoneNumber(promptUser("What is the phone number? "));
+        cellPhone.setOwner(promptUser("Who is the owner of the phone? "));
     }
 }

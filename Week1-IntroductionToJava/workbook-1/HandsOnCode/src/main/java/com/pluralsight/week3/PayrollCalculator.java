0abf10d663e595d076ee723cd69b304bc5ada988
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-09 13:44:01 -0700

    Payroll Calculator: bonus

--- Week1-IntroductionToJava/workbook-1/HandsOnCode/src/main/java/com/pluralsight/week3/PayrollCalculator.java
+++ Week1-IntroductionToJava/workbook-1/HandsOnCode/src/main/java/com/pluralsight/week3/PayrollCalculator.java
@@ -13,16 +13,30 @@ public class PayrollCalculator {
         String payrollFilename = promptPayrollFileName();
 
         try {
+            // Reader
             FileReader fileReader = new FileReader(fileName);
             BufferedReader bufferedReader = new BufferedReader(fileReader);
-            // this skips the first line. thanks Nima
-            String input = bufferedReader.readLine();
 
+            // Writer
             FileWriter fileWriter = new FileWriter(payrollFilename);
             BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-            bufferedWriter.write("id|name|gross pay\n");
 
-            while((input = bufferedReader.readLine()) != null){
+            // Skip reading the first line
+            String input = bufferedReader.readLine();
+
+            // Validate desired output format
+            boolean isJsonWanted = false;
+            if (payrollFilename.split(Pattern.quote("."))[1].equals("json")) {
+                isJsonWanted = true;
+            }
+
+            if (isJsonWanted) {
+                bufferedWriter.write("["); // for json format
+            } else {
+                bufferedWriter.write("id|name|gross pay\n");
+            }
+
+            while ((input = bufferedReader.readLine()) != null) {
 //                System.out.println(input);
                 String[] arr = input.split(Pattern.quote("|"));
                 int employeeId = Integer.parseInt(arr[0]);
@@ -31,25 +45,30 @@ public class PayrollCalculator {
                 double payRate = Double.parseDouble(arr[3]);
                 Employee employee = new Employee(employeeId, employeeName, hoursWorked, payRate);
 
-                bufferedWriter.append(String.format("%d|%s|%f\n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay()));
+                if (isJsonWanted) {
+                    //BONUS: write in json format
+                    bufferedWriter.append(String.format("\n\t{\"id\": %d, \"name\": \"%s\", \"grossPay\": %.2f},", employee.getEmployeeId(), employee.getName(), employee.getGrossPay()));
+                } else {
+                    bufferedWriter.append(String.format("%d|%s|%.2f\n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay()));
+                }
 //                System.out.printf("Employee with id %d, name: %s, gross pay: %.2f \n", employee.getEmployeeId(), employee.getName(), employee.getGrossPay());
-
             }
+            bufferedWriter.append("\n]");
 
             bufferedReader.close();
             bufferedWriter.close();
-        } catch (IOException e){
+        } catch (IOException e) {
             System.err.println(e);
         }
 
     } // end of main
 
-    public static String promptFileName(){
+    public static String promptFileName() {
         System.out.print("Enter the name of the file employee file to process: ");
         return scanner.nextLine();
     }
 
-    public static String promptPayrollFileName(){
+    public static String promptPayrollFileName() {
         System.out.print("Enter the name of the payroll file to create: ");
         return scanner.nextLine();
     }

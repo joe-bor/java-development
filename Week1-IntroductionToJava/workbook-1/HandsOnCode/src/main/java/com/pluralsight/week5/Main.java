0cf4f71627524fa950c84ab5eced8dbc421baa3c
Joezari Borlongan <bor.joezari@gmail.com>
2024-10-23 10:04:23 -0700

    tried fetching by pinging apis

--- Week1-IntroductionToJava/workbook-1/HandsOnCode/src/main/java/com/pluralsight/week5/Main.java
+++ Week1-IntroductionToJava/workbook-1/HandsOnCode/src/main/java/com/pluralsight/week5/Main.java
@@ -3,12 +3,29 @@ package com.pluralsight.week5;
 import org.slf4j.LoggerFactory;
 import org.slf4j.Logger;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
 
 public class Main {
     private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+    static String Url = "https://open.er-api.com/v6/latest/%s";
+    static final HttpClient CLIENT = HttpClient.newHttpClient();
 
     public static void main(String[] args) {
-        LOG.info("Hello");
-        LOG.warn("Warning");
+        LOG.info("----------------");
+        String currency = "CAD";
+
+        HttpRequest request = HttpRequest.newBuilder(URI.create(Url.formatted(currency))).build();
+
+        try {
+            HttpResponse<String> response = CLIENT.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println(response.body());
+        } catch (IOException | InterruptedException e) {
+            LOG.error("Failed fetching", e);
+        }
     }
 }
